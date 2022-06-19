815b63bdd321fed946437050d037e29e6a64ca9d
riicky-hub <richardjosephace1960@gmail.com>
2022-06-18 21:24:43 -0300

    adicionada paginas de cadastro e login

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
-
-route.get('/', (req, res) => {
-    res.render('index');
-})
+// Rota Home
+route.get('/', (req, res) => res.render('index'));
+// Rotas do Account
+route.get('/login', (req, res) => res.render('login'));
+route.get('/register', (req, res) => res.render('register'));
 
 module.exports = route;
