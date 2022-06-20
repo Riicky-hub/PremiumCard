5bd3e418cc98e6bae9a3cd0758920f29b80fd54c
riicky-hub <richardjosephace1960@gmail.com>
2022-06-19 23:15:24 -0300

    adicionado account page e painel pronto

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const route = express.Router();
+const accountController = require('./src/controllers/accountController');
 // Rota Home
 route.get('/', (req, res) => res.render('index'));
 // Rotas do Account
 route.get('/login', (req, res) => res.render('login'));
 route.get('/register', (req, res) => res.render('register'));
+route.get('/account', (req, res) => res.render('account'));
 
 module.exports = route;
